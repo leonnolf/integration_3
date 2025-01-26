df407362c0595cedb95a88d13c7b578c2b970e7a
leonnolf <leon.nolf@student.howest.be>
2025-01-26 15:37:41 +0100

    Add GSAP dependency and implement scroll-triggered animations for chapter content

--- src/script.js
+++ src/script.js
@@ -1,37 +1,30 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 document.addEventListener("DOMContentLoaded", function () {
-    
-    const progressBar = document.querySelector(".progress-bar");
-    window.addEventListener("scroll", () => {
-      const scrollTop = window.scrollY;
-      const docHeight = document.body.scrollHeight - window.innerHeight;
-      const progress = (scrollTop / docHeight) * 100;
-      progressBar.style.width = `${progress}%`;
-    });
   
-    
-    gsap.utils.toArray(".chapter, .slot").forEach((section) => {
-      gsap.from(section, {
-        scrollTrigger: {
-          trigger: section,
-          start: "top 80%",
-          end: "bottom 60%",
-          toggleActions: "play none none reverse",
-        },
-        opacity: 0,
-        y: 100,
-        duration: 1.5,
-        ease: "power3.out",
-      });
-    });
-  
-    gsap.from("header", {
-      scrollTrigger: {
-        trigger: "header",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-      },
-      y: -50,
-      opacity: 0,
-    });
+  const progressBar = document.querySelector(".progress-bar");
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.body.scrollHeight - window.innerHeight;
+    const progress = (scrollTop / docHeight) * 100;
+    progressBar.style.width = `${progress}%`;
   });
+
+  
+  gsap.utils.toArray(".chapter").forEach((section) => {
+    const content = section.querySelector(".chapter-content");
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none none reverse",
+      },
+    })
+      .to(content, { opacity: 1, display: "block", duration: 1 });
+  });
+});
